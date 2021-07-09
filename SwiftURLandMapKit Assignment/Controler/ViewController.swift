c2f654d9d0968b1bd2c64b840cc78fd175aa8543
Vishnu Nair <visbalak@hussain-khan.local>
2021-07-09 18:10:39 +0530

    Connected ViewController and ViewModel

--- SwiftURLandMapKit Assignment/Controler/ViewController.swift
+++ SwiftURLandMapKit Assignment/Controler/ViewController.swift
@@ -9,15 +9,46 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, RequestStatus {
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var mapKitView: MKMapView!
+    var viewModel: ViewModelOpeartions!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewModel = ViewModelController(_client: ApiHanlder(), _requestStatus: self)
+        viewModel.getData()
+    }
+    
+    func didSuccess() {
+        tableView.reloadData()
+    }
+    
+    func didFail() {
+        debugPrint("Failed Opeartion!!")
     }
 
-
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.dataModel!.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableCell = tableView.dequeueReusableCell(withIdentifier: "DataCell", for: indexPath) as! CarDeatilsTableViewCell
+        tableCell.seriesType.text = viewModel.dataModel![indexPath.row].vehicleDetails.series.rawValue
+        tableCell.makeName.text = viewModel.dataModel![indexPath.row].vehicleDetails.make.rawValue
+        tableCell.carName.text = viewModel.dataModel![indexPath.row].vehicleDetails.name
+        
+        
+        return tableCell
+    }
+    
+    
+}
+
 
