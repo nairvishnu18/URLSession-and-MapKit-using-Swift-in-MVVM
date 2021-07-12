6720699482aca1d0ef41182dc171ca38d4f8fadc
Vishnu Nair <visbalak@hussain-khan.local>
2021-07-12 13:12:05 +0530

    Added VehicleOperations and Vehicle service Class

--- SwiftURLandMapKit Assignment/ViewModel/ViewModel.swift
+++ SwiftURLandMapKit Assignment/ViewModel/ViewModel.swift
@@ -7,10 +7,17 @@
 //
 
 import Foundation
+import MapKit
 
 protocol ViewModelOpeartions {
     var dataModel: [CarDataModel]? {get}
     func getData()
+    func getRowCount() -> Int
+    
+}
+
+protocol VehicleOperations {
+    func locateVehicle(latitude: Double, longitude: Double) -> MKPointAnnotation
 }
 
 protocol RequestStatus: class {
@@ -19,23 +26,50 @@ protocol RequestStatus: class {
 }
 
 class ViewModelController: ViewModelOpeartions {
-    
+   
+
     var dataModel: [CarDataModel]?
-    private let apiHandler : ApiHanlder
+    private let apiHandler : ApiHandler
     weak var requestStatusDelegate: RequestStatus?
     
-    init(_client: ApiHanlder, _requestStatus: RequestStatus) {
-        apiHandler = _client
-        requestStatusDelegate = _requestStatus
+    init(_client: ApiHandler = ApiHandler(), _requestStatus: RequestStatus) {
+        self.apiHandler = _client
+        self.requestStatusDelegate = _requestStatus
     }
     
     
     func getData() {
         let requestURL = "http://www.mocky.io/v2/5dc3f2c13000003c003477a0"
         apiHandler.getAPIData(from: URL(string: requestURL)!, resultType: [CarDataModel].self) { [weak self] (carData) in
-            self?.dataModel = carData
-            self?.requestStatusDelegate?.didSuccess()
+            if carData.count != 0 {
+                //debugPrint(carData)
+                self?.dataModel = carData
+                self?.requestStatusDelegate?.didSuccess()
+                
+            } else  {
+                debugPrint("Error in assigning to Model")
+                self?.requestStatusDelegate?.didFail()
+            }
         }
     }
+    
+    func getRowCount() -> Int {
+        return dataModel?.count ?? 0
+    }
+    
+ 
+}
+
+class VehicleServices: VehicleOperations {
+    
+    
+    func locateVehicle(latitude: Double, longitude: Double) -> MKPointAnnotation {
+        debugPrint("Locating... \(latitude) & \(longitude)")
+        let annotations = MKPointAnnotation()
+        annotations.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        return annotations
+    }
+    
     
 }
