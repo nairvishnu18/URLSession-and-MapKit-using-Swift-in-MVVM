4f85a793d2efca0820fcb3c1e54b1977575dbd94
Vishnu Nair <visbalak@hussain-khan.local>
2021-07-12 21:03:53 +0530

    Added Map Annotations to ViewModel

--- SwiftURLandMapKit Assignment/ViewModel/ViewModel.swift
+++ SwiftURLandMapKit Assignment/ViewModel/ViewModel.swift
@@ -10,31 +10,32 @@ import Foundation
 import MapKit
 
 protocol ViewModelOpeartions {
+    
     var dataModel: [CarDataModel]? {get}
     func getData()
     func getRowCount() -> Int
+    func addAnnotationstoMap() -> [MKPointAnnotation]
+    func getRegionCoordinates() -> MKCoordinateRegion
     
 }
 
-protocol VehicleOperations {
-    func locateVehicle(latitude: Double, longitude: Double) -> MKPointAnnotation
-}
 
-protocol RequestStatus: class {
+protocol OutputDelegate: class {
     func didSuccess()
     func didFail()
 }
 
+
+
 class ViewModelController: ViewModelOpeartions {
-   
 
     var dataModel: [CarDataModel]?
     private let apiHandler : ApiHandler
-    weak var requestStatusDelegate: RequestStatus?
+    weak var outputDelegate: OutputDelegate?
     
-    init(_client: ApiHandler = ApiHandler(), _requestStatus: RequestStatus) {
+    init(_client: ApiHandler = ApiHandler(), _output: OutputDelegate) {
         self.apiHandler = _client
-        self.requestStatusDelegate = _requestStatus
+        self.outputDelegate = _output
     }
     
     
@@ -44,11 +45,11 @@ class ViewModelController: ViewModelOpeartions {
             if carData.count != 0 {
                 //debugPrint(carData)
                 self?.dataModel = carData
-                self?.requestStatusDelegate?.didSuccess()
+                self?.outputDelegate?.didSuccess()
                 
             } else  {
                 debugPrint("Error in assigning to Model")
-                self?.requestStatusDelegate?.didFail()
+                self?.outputDelegate?.didFail()
             }
         }
     }
@@ -57,19 +58,40 @@ class ViewModelController: ViewModelOpeartions {
         return dataModel?.count ?? 0
     }
     
- 
+    func addAnnotationstoMap() -> [MKPointAnnotation] {
+        var locations = [[String:Double]]()
+        dataModel?.forEach({ (coordinates) in
+            locations.append(
+                [
+                    "latitude":coordinates.location.latitude,
+                    "longitude": coordinates.location.longitude
+                ])
+        })
+        var pinAnnotations: [MKPointAnnotation] = []
+        for location in locations {
+            let annotations = MKPointAnnotation()
+            annotations.coordinate = CLLocationCoordinate2D(
+                latitude: location["latitude"]!,
+                longitude: location["longitude"]!
+            )
+            pinAnnotations.append(annotations)
+        }
+
+        return pinAnnotations
+    }
+
+    func getRegionCoordinates() -> MKCoordinateRegion {
+        let centerCoordinates = CLLocationCoordinate2D(latitude: 12.950415, longitude: 77.642605)
+//        let regionCoordinates = MKCoordinateRegion(
+//            center: centerCoordinates,
+//            span: MKCoordinateSpan(
+//                latitudeDelta: 0.1,
+//                longitudeDelta: 0.1))
+        
+        let mapCoordinates = MKCoordinateRegion(center: centerCoordinates, latitudinalMeters: 500, longitudinalMeters: 500)
+
+        return mapCoordinates
+    }
+
 }
 
-class VehicleServices: VehicleOperations {
-    
-    
-    func locateVehicle(latitude: Double, longitude: Double) -> MKPointAnnotation {
-        debugPrint("Locating... \(latitude) & \(longitude)")
-        let annotations = MKPointAnnotation()
-        annotations.coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        
-        return annotations
-    }
-    
-    
-}
